71913de7493d2df8309e95d780366133e44de0e7
FerdaKoplo <sahmuraivan43@gmail.com>
2025-10-25 01:15:26 +0700

    completing the controller

--- src/module/leaflet/leaflet.controller.ts
+++ src/module/leaflet/leaflet.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/common/guards/jwt-auth.guard";
 import { LeafletService } from "./leaflet.service";
+import { CreateLeafletDTO } from "./DTO'S/create-leaflet-dto.dto";
+import { UpdateLeafletDTO } from "./DTO'S/update-leaflet-dto.dto";
 
 @ApiTags('Leaflet')
 @Controller('leaflet')
@@ -9,5 +11,28 @@ import { LeafletService } from "./leaflet.service";
 export class LeafletController {
     constructor (private readonly leafletService : LeafletService) {}
 
-    
+    @Get()
+    async getLeaflets(){
+        return this.leafletService.getLeaflets()
+    }
+
+    @Get(':id')
+    async getDetailLeaflet(@Param('id', ParseIntPipe) id : number ){
+        return this.leafletService.getDetailLeaflet(id)
+    }
+
+    @Post()
+    async createLeaflet(@Body() dto : CreateLeafletDTO){
+        return this.leafletService.createLeaflet(dto)
+    }
+
+    @Put(':id')
+    async updateLeaflet(@Param('id', ParseIntPipe) id : number, @Body() dto : UpdateLeafletDTO ){
+        return this.leafletService.getDetailLeaflet(id)
+    }
+
+    @Delete(':id')
+    async deleteLeaflet(@Param('id', ParseIntPipe) id : number){
+        return this.leafletService.deleteLeaflet(id)
+    }
 }
